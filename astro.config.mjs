b769564e875daeb1ce465e11b42deea9b3f62f3a
Akkshay Sundara Rajan <sundar57@purdue.edu>
2026-05-21 19:09:52 +0530

    fix: astro audit issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { typst } from 'astro-typst';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ['lh3.googleusercontent.com']
+  },
   integrations: [typst()],
   vite: {
     plugins: [tailwindcss()]
